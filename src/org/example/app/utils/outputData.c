d1868a74bb2ae2b1a7e76ae212c583a86bcffa69
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-11-24 12:22:12 +0200

    update

--- src/org/example/app/utils/outputData.c
+++ src/org/example/app/utils/outputData.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-void print_matrix(int **arr, int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+void print_matrix(int **arr, int row, int colum) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < colum; j++) {
             printf("%4d ", arr[i][j]);
         }
         printf("\n");
